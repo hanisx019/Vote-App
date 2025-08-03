0b16ab9371ed36bb1ee2b4802451fe27b90cb5a0
hanisx019 <anisx019@gmail.com>
2025-08-04 02:07:02 +0530

    Add Files

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 const db = require('./db')
 
+const bodyParser=require('body-parser')
+app.use(bodyParser.json())
+
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(3000,()=>{
-    console.log("App Listening on Port 3000")
+// Importing user routes and using user routes
+const userRoutes = require('./routes/userRoute')
+app.use('/user',userRoutes)
+
+// Importing candidate routes and using candidate routes
+const candidateRoutes = require('./routes/candidateRoute')
+app.use('/candidate',candidateRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`App Listening on Port ${PORT}`)
 })
